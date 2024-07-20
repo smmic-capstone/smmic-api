cf32ff9ed7174f9c4cb2af373e65d3149195b0d0
jozuacydsagradojuariojcsj <jozuacydjuariojcsj@gmail.com>
2024-07-20 08:48:10 +0800

    Added MOdels register and login functions

---
+++ api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import *
+from djoser.serializers import UserSerializer, UserCreateSerializer
+from rest_framework.response import Response
+UserModel = get_user_model()
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = UserModel
+        fields=('UID','first_name','last_name','province','city','barangay','zone','zip_code','email','password')
+
+class CustomUserSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        model = UserModel
+        fields = ('UID','first_name','last_name','province','city','barangay','zone','zip_code','email')
+ 
